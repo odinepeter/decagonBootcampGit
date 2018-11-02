fbbe8102e52d20530f6825b3efdc7624130cff64
TOBIE <odinepeter@yahoo.com>
2018-11-02 09:13:08 +0100

    Initial commit

---
+++ dacagon.js
@@ -0,0 +1,17 @@
+function solution(P, C) {
+	var noOfGamePlayed = Maths.floor (P / 2);//can also use parseInt
+	if (noOfGamePlayed <= C) {
+		return noOfGamePlayed;
+	}else {
+		return C;
+	}
+}
+(solution(6, 12);
+(solution(6, 2);
+(solution(2, 9);
+(solution(2, 0);
+(solution(3, 1);
+(solution(7, 2);
+(solution(7, 9);
+(solution(99, 23);
+(solution(102, 102);
